fe388ad19c2b773fd0f05c330fa206d82c7018f7
wl_ubuntu <wl_ubuntu@Wl.com>
2024-06-29 22:30:49 +0800

    Use the history view configuration for hstr

--- zsh/hstr.zsh
+++ zsh/hstr.zsh
@@ -4,3 +4,4 @@ setopt histignorespace           # skip cmds w/ leading space from history
 export HSTR_CONFIG=hicolor       # get more colors
 bindkey -s "\C-r" "\C-a hstr -- \C-j"     # bind hstr to Ctrl-r (for Vi mode check doc)
 export HSTR_TIOCSTI=y
+export HSTR_CONFIG=raw-history-view
